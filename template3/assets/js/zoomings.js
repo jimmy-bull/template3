6df7e8b8c76da05679b55a33b0ded8329f3dc591
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-01-17 15:47:15 +0100

    Create zoomings.js

---
+++ template3/assets/js/zoomings.js
@@ -0,0 +1,48 @@
+//DESCRIPTION PAGES EFFECT ON MINIATURE CLICKS
+
+$(".miniature_images:eq(0)").css({
+    "border-style": "solid",
+    "padding": 0 + "px",
+    "border-width": 1 + "px",
+    'box-sizing': 'border-box',
+    'border-color': "#01FF70"
+})
+$(".miniature_images").hover(function (e) {
+    new ImageZoom(document.getElementById("img-container"), options);
+    $('.contain_big_miniature_images').attr('src', $(this).attr('src'))
+    $(this).css({
+        "border-style": "solid",
+        "padding": 0 + "px",
+        "border-width": 1 + "px",
+        'box-sizing': 'border-box',
+        'border-color': "#01FF70"
+    })
+    $(this).siblings().css({
+        "border-style": "none",
+    })
+    $('.js-image-zoom__zoomed-area').css({
+        'background': 'white',
+        'opacity': '-=0.1', //TO KEEP THE  Js-image-zoom__zoomed-area AT THE SAME PLACE
+    })
+
+})
+
+$("#img-container").hover(function (e) {
+    $('.the_other_side_of_zoom_part').css({
+        "opacity": 0
+    });
+})
+
+$("#img-container").mouseleave(function () {
+    $('.the_other_side_of_zoom_part').css({
+        "opacity": 1
+    });
+});
+
+$('.others_images:eq(0)').css({
+    "border-style": "solid",
+    "padding": 0 + "px",
+    "border-width": 1 + "px",
+    'box-sizing': 'border-box',
+    'border-color': "#01FF70"
+})
