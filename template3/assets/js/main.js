04b8cda6d7fe0b7dbb4ec5d0c23ed6ed7d85fef9
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-01-18 22:35:26 +0100

    Update main.js

--- template3/assets/js/main.js
+++ template3/assets/js/main.js
@@ -145,5 +145,19 @@ $(document).ready(function () {
     });
 });
 
+var owl_home2 = $('.owl_home2');
+owl_home2.owlCarousel();
+// Go to the next item
+$('.btn_right_carousel_home2').click(function () {
+    owl_home2.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.btn_left_carousel_home2').click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl_home2.trigger('prev.owl.carousel', [300]);
+})
+
+
 
 
