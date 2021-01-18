c321ed58625ef6df7dba93bcaa0a64d1e5b8620b
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-01-18 23:59:12 +0100

    Update main.js

--- template3/assets/js/main.js
+++ template3/assets/js/main.js
@@ -136,7 +136,7 @@ $(document).ready(function () {
                 nav: true
             },
             600: {
-                items: 3,
+                items: 2,
                 nav: false
             },
             1000: {
