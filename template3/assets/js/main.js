db0d7ab76cc0163b116ab302bf404edf41401d72
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-01-18 22:10:00 +0100

    Update main.js

--- template3/assets/js/main.js
+++ template3/assets/js/main.js
@@ -123,7 +123,26 @@ $('.close_main_menu').click(function (e) {
 })
 
 $(document).ready(function () {
-    $(".owl_home2").owlCarousel();
+    $(".owl_home2").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 });
 
 
